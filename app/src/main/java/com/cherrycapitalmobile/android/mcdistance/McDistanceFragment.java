bd4e6411f2545e1e13a34581e3e7b778ff5b6e1d
Andrew <A.J.Greenman@gmail.com>
2014-10-07 17:44:34 -0400

    Finished a basic UI for the McDistanceFragment class and wired it up in code.

--- app/src/main/java/com/cherrycapitalmobile/android/mcdistance/McDistanceFragment.java
+++ app/src/main/java/com/cherrycapitalmobile/android/mcdistance/McDistanceFragment.java
@@ -5,10 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class McDistanceFragment extends Fragment {
     TextView mMcDistanceTextView, mMilesTextView;
+    Button mViewMcDistanceButton, mCalculateDistanceButton, mGetDirectionsButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
@@ -16,6 +18,10 @@ public class McDistanceFragment extends Fragment {
 
         mMcDistanceTextView = (TextView) v.findViewById(R.id.mcdistanceTextView);
         mMilesTextView = (TextView) v.findViewById(R.id.milesTextView);
+
+        mViewMcDistanceButton = (Button) v.findViewById(R.id.view_mcdistance_button);
+        mCalculateDistanceButton = (Button) v.findViewById(R.id.calculate_distance_button);
+        mGetDirectionsButton = (Button) v.findViewById(R.id.directions_button);
 
         return v;
     }
