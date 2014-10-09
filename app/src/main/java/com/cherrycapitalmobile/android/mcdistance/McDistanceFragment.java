2a2a8e221de642c5281eed7e527abce2b057c716
Andrew <A.J.Greenman@gmail.com>
2014-10-09 15:53:52 -0400

    Added functionality for the phone to receive a location from gps and display the longitude and latitude values.

--- app/src/main/java/com/cherrycapitalmobile/android/mcdistance/McDistanceFragment.java
+++ app/src/main/java/com/cherrycapitalmobile/android/mcdistance/McDistanceFragment.java
@@ -1,6 +1,10 @@
 package com.cherrycapitalmobile.android.mcdistance;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,11 +13,26 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class McDistanceFragment extends Fragment {
+    private Location mLocation;
     private McDistanceManager mMcDistanceManager;
-    private TextView mMcDistanceTextView, mMilesTextView;
+    private TextView mMcDistanceTextView, mMilesTextView, mLatitudeTextView, mLongitudeTextView;
     private Button mViewMcDistanceButton, mCalculateDistanceButton, mGetDirectionsButton,
             mStartButton, mStopButton;
+
+    private BroadcastReceiver mLocationReceiver = new LocationReceiver() {
+
+        @Override
+        protected void onLocationReceived(Context context, Location loc) {
+            mLocation = loc;
+
+            if(isVisible()) {
+                updateUI();
+            }
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +48,8 @@ public class McDistanceFragment extends Fragment {
 
         mMcDistanceTextView = (TextView) v.findViewById(R.id.mcdistanceTextView);
         mMilesTextView = (TextView) v.findViewById(R.id.milesTextView);
+        mLatitudeTextView = (TextView) v.findViewById(R.id.latitudeTextView);
+        mLongitudeTextView = (TextView) v.findViewById(R.id.longitudeTextView);
 
         mViewMcDistanceButton = (Button) v.findViewById(R.id.view_mcdistance_button);
         mViewMcDistanceButton.setOnClickListener(new View.OnClickListener() {
@@ -85,8 +106,26 @@ public class McDistanceFragment extends Fragment {
         return v;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        getActivity().registerReceiver(mLocationReceiver,
+                new IntentFilter(McDistanceManager.ACTION_LOCATION));
+    }
+
+    @Override
+    public void onStop() {
+        getActivity().unregisterReceiver(mLocationReceiver);
+        super.onStop();
+    }
+
     private void updateUI() {
         boolean started = mMcDistanceManager.isTrackingLocation();
+
+        if(mLocation != null) {
+            mLatitudeTextView.setText(Double.toString(mLocation.getLatitude()));
+            mLongitudeTextView.setText(Double.toString(mLocation.getLongitude()));
+        }
 
         mStartButton.setEnabled(!started);
         mStopButton.setEnabled(started);
